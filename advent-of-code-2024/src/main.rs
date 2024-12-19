a3e186915ee132e24001e36698ae395a46881a56
Nathan Specht <nathanspecht@gmail.com>
2024-12-19 13:44:18 -0800

    add day 3

--- advent-of-code-2024/src/main.rs
+++ advent-of-code-2024/src/main.rs
@@ -8,6 +8,8 @@ fn main() {
         "1B" => solutions::day01_b::run(),
         "2A" => solutions::day02_a::run(),
         "2B" => solutions::day02_b::run(),
+        "3A" => solutions::day03_a::run(),
+        "3B" => solutions::day03_b::run(),
         _ => println!("No solution found for {}", CURRENT),
     }
 }
--- advent-of-code-2024/src/solutions.rs
+++ advent-of-code-2024/src/solutions.rs
@@ -2,3 +2,5 @@ pub mod day01_a;
 pub mod day01_b;
 pub mod day02_a;
 pub mod day02_b;
+pub mod day03_a;
+pub mod day03_b;
