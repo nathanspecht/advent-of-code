5fe20a851d5289e1da4c1636211fb50e594c2d00
Nathan Specht <nathanspecht@gmail.com>
2024-12-25 12:39:28 -0600

    complete day 7

--- advent-of-code-2024/src/solutions/day07_a.rs
+++ advent-of-code-2024/src/solutions/day07_a.rs
@@ -1,6 +1,41 @@
 use std::fs;
 
 pub fn run() {
-    let _input = fs::read_to_string("src/inputs/day07_a.txt").expect("Failed to read file");
-    println!("day07_a");
+    let input = fs::read_to_string("src/inputs/day07_a.txt").expect("Failed to read file");
+
+    let mut sum = 0;
+
+    input.lines().for_each(|line| {
+        let (total, values) = {
+            let mut iter = line.trim().split(": ");
+            let total = iter.next().unwrap().parse::<i64>().unwrap();
+            let values = iter
+                .next()
+                .unwrap()
+                .split(" ")
+                .map(|x| x.parse::<i64>().unwrap())
+                .collect::<Vec<i64>>();
+
+            (total, values)
+        };
+
+        if is_viable(total, 0, &values) {
+            sum += total;
+        }
+    });
+
+    println!("{}", sum);
+}
+
+fn is_viable(target: i64, current: i64, values: &Vec<i64>) -> bool {
+    if values.len() == 0 {
+        return target == current;
+    }
+
+    let next = values[0];
+
+    let remaining = values[1..].to_vec();
+
+    return is_viable(target, current + next, &remaining)
+        || is_viable(target, current * next, &remaining);
 }
--- advent-of-code-2024/src/solutions/day07_b.rs
+++ advent-of-code-2024/src/solutions/day07_b.rs
@@ -1,6 +1,44 @@
 use std::fs;
 
 pub fn run() {
-    let _input = fs::read_to_string("src/inputs/day07_b.txt").expect("Failed to read file");
-    println!("day07_b");
+    let input = fs::read_to_string("src/inputs/day07_a.txt").expect("Failed to read file");
+
+    let mut sum = 0;
+
+    input.lines().for_each(|line| {
+        let (total, values) = {
+            let mut iter = line.trim().split(": ");
+            let total = iter.next().unwrap().parse::<i64>().unwrap();
+            let values = iter
+                .next()
+                .unwrap()
+                .split(" ")
+                .map(|x| x.parse::<i64>().unwrap())
+                .collect::<Vec<i64>>();
+
+            (total, values)
+        };
+
+        if is_viable(total, 0, &values) {
+            sum += total;
+        }
+    });
+
+    println!("{}", sum);
+}
+
+fn is_viable(target: i64, current: i64, values: &Vec<i64>) -> bool {
+    if values.len() == 0 {
+        return target == current;
+    }
+
+    let next = values[0];
+
+    let remaining = values[1..].to_vec();
+
+    let concat = format!("{current}{next}").parse::<i64>().unwrap();
+
+    return is_viable(target, current + next, &remaining)
+        || is_viable(target, current * next, &remaining)
+        || is_viable(target, concat, &remaining);
 }
