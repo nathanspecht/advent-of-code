1bbcdf6aee2f17dd36cf354ac7c4aedda8a7a26e
Nathan Specht <nathanspecht@gmail.com>
2025-12-10 22:40:00 -0800

    try bottom up search

--- advent-of-code-2025/src/solutions/day07_b.rs
+++ advent-of-code-2025/src/solutions/day07_b.rs
@@ -1,35 +1,86 @@
-use crate::grid::{Grid, DOWN, DOWNLEFT, DOWNRIGHT};
+use crate::grid::{Grid, DOWN, DOWNLEFT, DOWNRIGHT, LEFT, RIGHT, UP};
 use crate::util::read_to_grid;
 
 pub fn run() {
-    let input = read_to_grid("src/inputs/day07_b.txt");
-    let grid = Grid::from(input);
-    let (x, y) = grid.position('S').unwrap();
+    let input = read_to_grid("src/inputs/day07_a.txt");
+    let mut grid = Grid::from(input);
 
-    let count = go_forward(&grid, x, y);
+    let mut count = 0;
 
-    println!("{}", count);
-}
-
-fn go_forward(grid: &Grid<char>, x: isize, y: isize) -> u64 {
-    if !grid.in_bounds(x, y) {
-        return 1;
+    for y in 0..grid.height {
+        for x in 0..grid.width {
+            match grid.get(x, y) {
+                Some('S') => {
+                    if grid.neighbor(x, y, DOWN) == Some(&'.') {
+                        grid.set_neighbor(x, y, DOWN, '|');
+                    }
+                }
+                Some('^') => {
+                    if grid.neighbor(x, y, UP) == Some(&'|') {
+                        count += 1;
+                        grid.set_neighbor(x, y, LEFT, '|');
+                        grid.set_neighbor(x, y, RIGHT, '|');
+                    }
+                }
+                Some('.') => {
+                    if grid.neighbor(x, y, UP) == Some(&'|') {
+                        grid.set(x, y, '|');
+                    }
+                }
+                _ => {}
+            }
+        }
     }
 
-    match grid.get(x, y).unwrap() {
-        '.' | 'S' => {
-            let (x1, y1) = DOWN + (x, y);
+    grid.print();
 
-            return go_forward(grid, x1, y1);
+    for x in 0..grid.width {
+        if grid.get(x, grid.height - 1) == Some(&'|') {
+            let (x1, y1) = UP + (x, grid.height - 1);
+            count += follow_path(&grid, x1, y1);
         }
-        '^' => {
-            let (x1, y1) = DOWNLEFT + (x, y);
-            let (x2, y2) = DOWNRIGHT + (x, y);
+    }
 
-            return go_forward(grid, x1, y1) + go_forward(grid, x2, y2);
+    println!("{count}");
+}
+
+fn follow_path(grid: &Grid<char>, x: isize, y: isize) -> u64 {
+    match grid.get(x, y) {
+        Some('S') => {
+            return 1;
+        }
+        Some('|') | Some('^') => {
+            let (x1, y1) = UP + (x, y);
+            return follow_path(grid, x1, y1);
+        }
+        Some('.') => {
+            match (
+                grid.neighbor(x, y, DOWNLEFT),
+                grid.neighbor(x, y, DOWNRIGHT),
+            ) {
+                (Some('^'), Some('^')) => {
+                    let (x1, y1) = DOWNLEFT + (x, y);
+                    let (x2, y2) = DOWNRIGHT + (x, y);
+
+                    return follow_path(grid, x1, y1) + follow_path(grid, x2, y2);
+                }
+                (Some('^'), _) => {
+                    let (x1, y1) = DOWNLEFT + (x, y);
+
+                    return follow_path(grid, x1, y1);
+                }
+                (_, Some('^')) => {
+                    let (x1, y1) = DOWNRIGHT + (x, y);
+
+                    return follow_path(grid, x1, y1);
+                }
+                _ => {
+                    panic!("Invalid path");
+                }
+            }
         }
         _ => {
-            panic!("Unexpected character: {}", grid.get(x, y).unwrap());
+            panic!("Invalid character");
         }
     }
 }
