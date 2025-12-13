4bc0cb0e6916fb263de77d11f13d3e0dd57e4873
Nathan Specht <nathanspecht@gmail.com>
2025-12-12 16:11:43 -0800

    brute force, check every point in rectangle

--- advent-of-code-2025/src/solutions/day09_b.rs
+++ advent-of-code-2025/src/solutions/day09_b.rs
@@ -16,20 +16,47 @@ fn check_intersect((h1, h2): &(Point, Point), (v1, v2): &(Point, Point)) -> bool
     (x_range.0 <= x && x <= x_range.1) && (y_range.0 <= y && y <= y_range.1)
 }
 
-fn get_center(points: &[Point; 4]) -> Point {
-    let sum_x: i64 = points.iter().map(|p| p.x).sum();
-    let sum_y: i64 = points.iter().map(|p| p.y).sum();
+fn is_inside(point: &Point, vertical_segments: &Vec<(Point, Point)>) -> bool {
+    let num_right = vertical_segments
+        .iter()
+        .filter(|segment| {
+            let horizontal_point = Point {
+                y: point.y,
+                x: i64::MAX,
+            };
 
-    Point {
-        x: sum_x / 4,
-        y: sum_y / 4,
+            return check_intersect(&(point.clone(), horizontal_point), &segment);
+        })
+        .count();
+
+    num_right % 2 != 0
+}
+
+fn is_on_segment(
+    point: &Point,
+    vertical_segments: &Vec<(Point, Point)>,
+    horizontal_segments: &Vec<(Point, Point)>,
+) -> bool {
+    for segment in vertical_segments {
+        if check_intersect(&(point.clone(), point.clone()), segment) {
+            return true;
+        }
     }
+
+    for segment in horizontal_segments {
+        if check_intersect(segment, &(point.clone(), point.clone())) {
+            return true;
+        }
+    }
+
+    return false;
 }
 
 pub fn run() {
-    let input = fs::read_to_string("src/inputs/day09_a.txt").expect("Failed to read file");
+    let input = fs::read_to_string("src/inputs/day09_b.txt").expect("Failed to read file");
     let mut coords: Vec<Point> = vec![];
     let mut vertical_segments: Vec<(Point, Point)> = vec![];
+    let mut horizontal_segments: Vec<(Point, Point)> = vec![];
 
     for line in input.lines() {
         let (a, b) = line.split_once(',').unwrap();
@@ -50,6 +77,8 @@ pub fn run() {
 
         if prev.x == curr.x {
             vertical_segments.push((prev, curr));
+        } else if prev.y == curr.y {
+            horizontal_segments.push((prev, curr));
         }
     }
 
@@ -57,47 +86,35 @@ pub fn run() {
     let mut max = 0;
 
     for a in &coords {
-        for b in &coords {
+        'inner: for b in &coords {
             let width = a.x.abs_diff(b.x) + 1;
             let height = a.y.abs_diff(b.y) + 1;
             let area = width * height;
 
-            let corners = [
-                a.clone(),
-                b.clone(),
-                Point { x: a.x, y: b.y },
-                Point { x: b.x, y: a.y },
-            ];
-
-            let center = get_center(&corners);
-
-            let is_inside = corners
-                .iter()
-                .chain(std::iter::once(&center))
-                .all(|corner| {
-                    if coords.contains(corner) {
-                        return true;
-                    }
-
-                    let num_right = vertical_segments
-                        .iter()
-                        .filter(|segment| {
-                            let horizontal_point = Point {
-                                y: corner.y,
-                                x: i64::MAX,
-                            };
-
-                            check_intersect(&(corner.clone(), horizontal_point), &segment)
-                        })
-                        .count();
-
-                    num_right % 2 != 0
-                });
-
-            if area > max && is_inside {
-                max = area;
-                max_string = format!("{:?}, {:?}", a, b);
+            if area <= max {
+                continue 'inner;
             }
+
+            let label = format!("{:?}, {:?}", a, b);
+
+            let x_min = a.x.min(b.x);
+            let x_max = a.x.max(b.x);
+            let y_min = a.y.min(b.y);
+            let y_max = a.y.max(b.y);
+
+            for x in x_min..=x_max {
+                for y in y_min..=y_max {
+                    if !coords.contains(&Point { x, y })
+                        && !is_on_segment(&Point { x, y }, &vertical_segments, &horizontal_segments)
+                        && !is_inside(&Point { x, y }, &vertical_segments)
+                    {
+                        continue 'inner;
+                    }
+                }
+            }
+
+            max = area;
+            max_string = label;
         }
     }
 
